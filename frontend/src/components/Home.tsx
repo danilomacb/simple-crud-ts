87486454359f1f7c3e32464845b9326e603ab517
danilomacb <danilo.bakun@gmail.com>
2020-05-14 10:12:58 -0300

    trying post on frontend

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
 
 function Home() {
+  const create = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(event.currentTarget.firstChild)
+  };
+
   return (
     <Container className="mt-5">
       <Row>
         <Col sm={6} md={4} lg={3} className="p-2">
-          <Form>
+          <Form onSubmit={create}>
             <Form.Control type="text" placeholder="New" />
           </Form>
         </Col>
